24cbb328485868fd1235024366cd406a8b8fba14
Tochi999 <lgorg19@freeuni.edu.ge>
2023-02-07 16:09:48 +0400

    implemented IUserRepository in inmemory database #four tests fails #implement get_user_id_by_api_key(), for tests to pass.

--- app/infra/in_memory/in_memory.py
+++ app/infra/in_memory/in_memory.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import DefaultDict
+from typing import DefaultDict, Optional, List
 
 from app.core.transactions.interactor import Transaction
 from app.core.users.interactor import User
@@ -15,3 +15,29 @@ class InMemoryRepository:
         self._users = defaultdict()
         self._wallets = defaultdict()
         self._transactions = defaultdict()
+
+    # USER
+    def add_user(self, new_user: User) -> bool:  # api_key is always unique
+        if new_user in list(self._users.values()):
+            return False
+        else:
+            self._users[new_user.get_user_id()] = new_user
+            return True
+
+    def get_user_by_username(self, username: str) -> Optional[User]:
+        for _, current_user in self._users.items():
+            if current_user.get_username() == username:
+                return current_user
+
+        return None
+
+    def get_user_by_id(self, user_id: int) -> Optional[User]:
+        return self._users.get(user_id, None)
+
+    def get_all_users(self) -> List[User]:
+        return list(self._users.values())
+
+    def get_max_user_id(self) -> int:
+        return len(self._users)
+
+
---
+++ tests/core/test_users/test_user_by_core.py
@@ -0,0 +1,104 @@
+from typing import Optional
+
+import pytest
+
+from app.core.facade import BitcoinWalletCore, UserResponse
+from app.core.users.interactor import User
+from app.infra.utils.hasher import DefaultHashFunction
+from app.infra.in_memory.in_memory import InMemoryRepository
+
+
+@pytest.fixture
+def core() -> BitcoinWalletCore:
+    repository: InMemoryRepository = InMemoryRepository()
+    return BitcoinWalletCore.create(
+        users_repository=repository,
+    )
+
+
+@pytest.fixture
+def user() -> User:
+    username: str = "Lean"
+    password: str = "password"
+
+    return User(_user_id=0, _username=username, _password=password)
+
+
+@pytest.fixture
+def hasher() -> DefaultHashFunction:
+    hasher: DefaultHashFunction = DefaultHashFunction()
+    return hasher
+
+
+def test_if_user_gets_saved(core: BitcoinWalletCore, user: User) -> None:
+    response: UserResponse = core.register_user(
+        user.get_username(), user.get_password()
+    )
+    assert core.get_user_id_by_api_key(api_key=response.api_key) != -1
+
+
+def test_if_user_gets_saved_again(core: BitcoinWalletCore, user: User) -> None:
+    response: UserResponse = core.register_user(
+        user.get_username(), user.get_password()
+    )
+    assert core.get_user_id_by_api_key(api_key=response.api_key) != -1
+
+    response: UserResponse = core.register_user(
+        user.get_username(), user.get_password()
+    )
+    assert core.get_user_id_by_api_key(api_key=response.api_key) == -1
+
+
+def test_valid_api_key_response(core: BitcoinWalletCore, user: User) -> None:
+    response: UserResponse = core.register_user(
+        user.get_username(), user.get_password()
+    )
+    assert core.get_user_id_by_api_key(api_key=response.api_key) != -1
+
+
+def test_invalid_api_key_response(core: BitcoinWalletCore, user: User) -> None:
+    response: UserResponse = core.register_user(
+        user.get_username(), user.get_password()
+    )
+    assert core.get_user_id_by_api_key(api_key=response.api_key + "12") == -1
+
+
+def test_get_user(core: BitcoinWalletCore, user: User) -> None:
+    core.register_user(user.get_username(), user.get_password())
+    user_from_database: Optional[User] = core.get_user(user.get_username())
+
+    assert user_from_database is not None
+
+
+def test_get_user_neg_wrong_username(core: BitcoinWalletCore, user: User) -> None:
+    core.register_user(user.get_username(), user.get_password())
+    user_from_database: Optional[User] = core.get_user(user.get_password())
+
+    assert user_from_database is None
+
+
+def test_api_key_prefix(core: BitcoinWalletCore, user: User) -> None:
+    response: UserResponse = core.register_user(
+        user.get_username(), user.get_password()
+    )
+    api_key: str = response.api_key
+    assert api_key.startswith("api_key_")
+
+
+def test_password_gets_hashed(core: BitcoinWalletCore, user: User) -> None:
+    core.register_user(user.get_username(), user.get_password())
+    user_from_database: Optional[User] = core.get_user(user.get_username())
+    assert user_from_database is not None
+    assert user.get_password() != user_from_database.get_password()
+
+
+def test_password_gets_hashed_correctly(
+        core: BitcoinWalletCore, user: User, hasher: DefaultHashFunction
+) -> None:
+    core.register_user(user.get_username(), user.get_password())
+    user_from_database: Optional[User] = core.get_user(user.get_username())
+
+    assert user_from_database is not None
+    hashed_password = hasher(user.get_password())
+
+    assert user_from_database.get_password() == hashed_password
