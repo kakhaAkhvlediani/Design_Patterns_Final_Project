a422a524c440e8fdc5a4fa5010eeff26f54ba52b
Kakhaber Akhvlediani <90695828+kakhaAkhvlediani@users.noreply.github.com>
2023-02-09 01:13:23 +0400

    Update api_keys_repository.py

--- app/infra/SQLlite/api_keys_repository.py
+++ app/infra/SQLlite/api_keys_repository.py
@@ -1,32 +1,36 @@
-import pytest
-
-from app.core.wallets.interactor import Wallet
+import sqlite3 as database
 
 
-@pytest.fixture
-def wallet() -> Wallet:
-    return Wallet(
-        _user_id=10,
-        _address="wallet_address_1",
-        _balance_btc=10.7,
-    )
+class SQLAPIKeysRepository:
+    def __init__(self) -> None:
+        self._database_name = "bitcoin.db"
+        with database.connect(self._database_name) as conn:
+            c = conn.cursor()
 
+            c.execute(
+                """
+            CREATE TABLE IF NOT EXISTS api_keys (
+                user_id INTEGER,
+                api_key TEXT UNIQUE,
+                FOREIGN KEY (user_id) REFERENCES users(id)
+            )
+            """
+            )
 
-def test_owner(wallet: Wallet) -> None:
-    assert wallet.get_owner_id() == 10
+    def add_api_key_id_pair(self, api_key: str, user_id: int) -> None:
+        with database.connect(self._database_name) as conn:
+            c = conn.cursor()
+            c.execute(
+                "INSERT INTO api_keys (user_id, api_key) VALUES (?, ?)",
+                (user_id, api_key),
+            )
 
-
-def test_address(wallet: Wallet) -> None:
-    assert wallet.get_address() == "wallet_address_1"
-
-
-def test_balance(wallet: Wallet) -> None:
-    assert wallet.get_balance_in_btc() == 10.7
-
-
-def test_equals_method(wallet: Wallet) -> None:
-    assert wallet == Wallet(
-        _user_id=10,
-        _address="wallet_address_1",
-        _balance_btc=10.7,
-    )
+    def get_user_id_by_api_key(self, api_key: str) -> int:
+        with database.connect(self._database_name) as conn:
+            c = conn.cursor()
+            c.execute("SELECT user_id FROM api_keys WHERE api_key=?", (api_key,))
+            result = c.fetchone()
+            if result is not None:
+                return int(result[0])
+            else:
+                return -1
