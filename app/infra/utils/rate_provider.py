0f6a335b8da4a4edc8253cafbadd1908fcd5e33d
kakhaAkhvlediani <kakhv20@freeuni.edu.ge>
2023-02-09 01:39:59 +0400

    Adding fee_strategy.py class and changing facade

--- app/infra/utils/rate_provider.py
+++ app/infra/utils/rate_provider.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from typing import Any
+
+import requests
 
 
 @dataclass
@@ -7,3 +10,15 @@ class DefaultRateProvider:
 
     def get_exchange_rate(self) -> float:
         return self._exchange_rate
+
+
+@dataclass
+class ExchangeRateProvider:
+    source: str = "https://api.coindesk.com/v1/bpi/currentprice/USD.json"
+
+    def get_exchange_rate(self) -> float:
+        result: Any = requests.get(self.source).json()["bpi"]["USD"]["rate_float"]
+        if isinstance(result, float):
+            return result
+        else:
+            return 1
