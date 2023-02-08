c706c91dd05937e7f60baa52155377b5ddbc5ef4
Tochi999 <lgorg19@freeuni.edu.ge>
2023-02-08 14:56:14 +0400

    implementing use of wallet interactor needs testcases

--- app/core/facade.py
+++ app/core/facade.py
@@ -1,13 +1,17 @@
+import uuid
 from asyncio import Protocol
-from dataclasses import dataclass
-from typing import Optional
+from dataclasses import dataclass, field
+from typing import Optional, Dict, Any, List
 
 from starlette import status
 
 from app.core.api_key.interactor import APIKeyInteractor, IAPIKeyRepository
+from app.core.transactions.interactor import Transaction
 from app.core.users.interactor import UsersInteractor, IUserRepository, User
-from app.core.wallets.interactor import IWalletRepository, WalletsInteractor
+from app.core.wallets.interactor import IWalletRepository, WalletsInteractor, Wallet
 from app.infra.utils.hasher import DefaultHashFunction
+
+MAX_NUM_OF_WALLET = 3  # todo add constants class
 
 
 @dataclass
@@ -15,6 +19,13 @@ class UserResponse:
     status: int
     msg: str = ""
     api_key: str = ""
+
+@dataclass
+class WalletResponse:
+    status: int
+    msg: str = ""
+    wallet_info: Dict[str, Any] = field(default_factory=lambda: {})
+    transactions: List[Dict[str, Any]] = field(default_factory=lambda: [])
 
 
 class IHasher(Protocol):
@@ -74,4 +85,107 @@ class BitcoinWalletCore:
 
         return UserResponse(
             status=status.HTTP_201_CREATED, msg="Registered", api_key=api_key
+        )
+
+    # Wallet RESPONSE
+    def _generate_wallet_address(self) -> str:
+        return "wallet_" + str(uuid.uuid4())[0:12]
+
+    def _check_if_user_has_max_num_of_wallets(self, user_id: int) -> bool:
+        return (
+                self._wallets_interactor.get_number_of_wallets_of_user(user_id)
+                == MAX_NUM_OF_WALLET
+        )
+
+    def _calculate_exchange_value_in_usd(self, amount_in_btc: float) -> float:
+        return amount_in_btc * self._rate_provider.get_exchange_rate()
+
+    def _calculate_exchange_value_in_btc(self, amount_in_usd: float) -> float:
+        return amount_in_usd / self._rate_provider.get_exchange_rate()
+
+    def create_wallet(self, api_key: str) -> WalletResponse:
+        user_id: int = self.get_user_id_by_api_key(api_key)
+        address: str = self._generate_wallet_address()
+
+        if user_id == -1:
+            return WalletResponse(
+                status=status.HTTP_403_FORBIDDEN, msg="Invalid api_key"
+            )
+
+        if self._check_if_user_has_max_num_of_wallets(user_id=user_id):
+            return WalletResponse(
+                status=status.HTTP_400_BAD_REQUEST,
+                msg="User has reached the max number of wallets",
+            )
+
+        wallet: Wallet = self._wallets_interactor.create_wallet(
+            user_id=user_id,
+            address=address,
+            balance_in_btc=self._wallet_starting_balance,
+        )
+
+        to_dict: Dict[str, Any] = wallet.to_dict()
+        to_dict["balance_in_usd"] = self._calculate_exchange_value_in_usd(
+            amount_in_btc=wallet.get_balance_in_btc()
+        )
+
+        return WalletResponse(
+            status=status.HTTP_201_CREATED,
+            msg="Wallet created",
+            wallet_info=to_dict,
+        )
+
+    def get_wallet(self, api_key: str, address: str) -> WalletResponse:
+        user_id: int = self.get_user_id_by_api_key(api_key)
+
+        if user_id == -1:
+            return WalletResponse(
+                status=status.HTTP_403_FORBIDDEN, msg="Invalid api_key"
+            )
+
+        wallet: Optional[Wallet] = self._wallets_interactor.get_wallet(address=address)
+        if wallet is None:
+            return WalletResponse(
+                status=status.HTTP_404_NOT_FOUND, msg="No wallet with this address"
+            )
+        elif user_id != wallet.get_owner_id():
+            return WalletResponse(
+                status=status.HTTP_403_FORBIDDEN, msg="Wrong api_key for this wallet"
+            )
+
+        to_dict: Dict[str, Any] = wallet.to_dict()
+        to_dict["balance_in_usd"] = self._calculate_exchange_value_in_usd(
+            amount_in_btc=wallet.get_balance_in_btc()
+        )
+
+        return WalletResponse(
+            status=status.HTTP_200_OK, msg="Returning Wallet", wallet_info=to_dict
+        )
+
+    def get_transactions_of_wallet(self, api_key: str, address: str) -> WalletResponse:
+        user_id: int = self.get_user_id_by_api_key(api_key)
+        if user_id == -1:
+            return WalletResponse(
+                status=status.HTTP_403_FORBIDDEN, msg="Invalid api_key"
+            )
+
+        wallet: Optional[Wallet] = self._wallets_interactor.get_wallet(address=address)
+        if wallet is None:
+            return WalletResponse(
+                status=status.HTTP_404_NOT_FOUND, msg="Wallet not found"
+            )
+        elif wallet.get_owner_id() != user_id:
+            return WalletResponse(status=status.HTTP_403_FORBIDDEN, msg="Wrong api_key")
+
+        transactions: List[
+            Transaction
+        ] = self._transactions_interactor.get_transactions_of_wallet(address)
+        transactions_to_send_back: List[Dict[str, Any]] = [
+            transaction.to_dict() for transaction in transactions
+        ]
+
+        return WalletResponse(
+            status=status.HTTP_200_OK,
+            msg="Transactions have been acquired",
+            transactions=transactions_to_send_back,
         )
