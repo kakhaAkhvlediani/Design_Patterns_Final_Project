ddd65c6f08d2aba61c36be1959ac9e694bb61bb4
kakhaAkhvlediani <kakhv20@freeuni.edu.ge>
2023-02-08 14:26:29 +0400

    Add APIKEY-ID to database

--- app/infra/in_memory/in_memory.py
+++ app/infra/in_memory/in_memory.py
@@ -15,6 +15,7 @@ class InMemoryRepository:
         self._users = defaultdict()
         self._wallets = defaultdict()
         self._transactions = defaultdict()
+        self._api_keys_user_ids = defaultdict()
 
     # USER
     def add_user(self, new_user: User) -> bool:  # api_key is always unique
@@ -40,4 +41,9 @@ class InMemoryRepository:
     def get_max_user_id(self) -> int:
         return len(self._users)
 
+    # API_KEY
+    def add_api_key_id_pair(self, api_key: str, user_id: int) -> None:
+        self._api_keys_user_ids[api_key] = user_id
 
+    def get_user_id_by_api_key(self, api_key: str) -> int:
+        return self._api_keys_user_ids.get(api_key, -1)
