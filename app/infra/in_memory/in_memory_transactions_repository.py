1270b1a17da0622555cfe3aaec4c1dc13430ed39
Khabju <tbark19@freeuni.edu.ge>
2023-02-08 20:05:51 +0400

    Added Interactor class with tests

--- app/infra/in_memory/in_memory_transactions_repository.py
+++ app/infra/in_memory/in_memory_transactions_repository.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import DefaultDict
+from typing import DefaultDict, List
 
 from app.core.transactions.interactor import Transaction
 
@@ -9,3 +9,21 @@ class InMemoryAPIKeyRepository:
 
     def __init__(self) -> None:
         self._api_keys_user_ids = defaultdict()
+
+    # TRANSACTION
+    def add_transaction(self, transaction: Transaction) -> None:
+        self._transactions[len(self._transactions)] = transaction
+
+    def get_wallet_transactions(self, address: str) -> List[Transaction]:
+        transactions: List[Transaction] = []
+        for _, transaction in self._transactions.items():
+            if (
+                    transaction.get_from_address() == address
+                    or transaction.get_to_address() == address
+            ):
+                transactions.append(transaction)
+
+        return transactions
+
+    def get_all_transactions(self) -> List[Transaction]:
+        return list(self._transactions.values())
