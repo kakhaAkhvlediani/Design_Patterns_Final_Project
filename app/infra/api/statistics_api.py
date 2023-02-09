2320fcc30aeebd6e94a0112dbee9c80d2c6a2d3a
Tochi999 <lgorg19@freeuni.edu.ge>
2023-02-09 19:22:58 +0400

    update statistics_api and transactions_api

--- app/infra/api/statistics_api.py
+++ app/infra/api/statistics_api.py
@@ -1,17 +1,20 @@
 # `GET /statistics`
 #   - Requires pre-set (hard coded) Admin API key
 #   - Returns the total number of transactions and platform profit
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 
-from app.core.facade import BitcoinWalletCore
+from app.core.facade import BitcoinWalletCore, StatisticsResponse
 from app.infra.api.dependables import get_core
 
 statistics_api: APIRouter = APIRouter()
 
 
-@statistics_api.get("/statistics", status_code=status.HTTP_200_OK)
+@statistics_api.get("/statistics")
 def get_statistics(
     admin_api_key: str, core: BitcoinWalletCore = Depends(get_core)
-) -> str:
-    return "response"
+) -> StatisticsResponse:
+    response: StatisticsResponse = core.get_statistics(admin_api_key=admin_api_key)
+    if response.status != status.HTTP_200_OK:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
--- app/infra/api/transactions_api.py
+++ app/infra/api/transactions_api.py
@@ -5,21 +5,29 @@
 #   - System takes a 1.5% (of the transferred amount)
 #   fee for transfers to the foreign wallets
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 
-from app.core.facade import BitcoinWalletCore
+from app.core.facade import BitcoinWalletCore, TransactionResponse
 from app.infra.api.dependables import get_core
 
 transactions_api: APIRouter = APIRouter()
 
 
-@transactions_api.post("/transactions", status_code=status.HTTP_201_CREATED)
+@transactions_api.post("/transactions")
 def make_transaction(
     api_key: str,
     from_address: str,
     to_address: str,
     amount: float,
     core: BitcoinWalletCore = Depends(get_core),
-) -> str:
-    return "response"
+) -> TransactionResponse:
+    response: TransactionResponse = core.make_transaction(
+        api_key=api_key,
+        from_address=from_address,
+        to_address=to_address,
+        amount=amount,
+    )
+    if response.status != status.HTTP_201_CREATED:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
