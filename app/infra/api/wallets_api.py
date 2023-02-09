955b5fc61157b3c91114cac3fc515fc8d0c0b9fe
kakhaAkhvlediani <kakhv20@freeuni.edu.ge>
2023-02-09 22:32:38 +0400

    user_api.py and wallets_api.py

--- app/infra/api/wallets_api.py
+++ app/infra/api/wallets_api.py
@@ -13,10 +13,10 @@
 # `GET /wallets/{address}/transactions`
 #   - Requires API key
 #   - returns transactions related to the wallet
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 
-from app.core.facade import BitcoinWalletCore
+from app.core.facade import BitcoinWalletCore, WalletResponse
 from app.infra.api.dependables import get_core
 
 wallets_api: APIRouter = APIRouter()
@@ -24,25 +24,70 @@ wallets_api: APIRouter = APIRouter()
 
 @wallets_api.post("/wallets", status_code=status.HTTP_201_CREATED)
 def create_wallet(
-    api_key: str,
-    core: BitcoinWalletCore = Depends(get_core),
-) -> str:
-    return "response"
+        api_key: str,
+        core: BitcoinWalletCore = Depends(get_core),
+) -> WalletResponse:
+    response: WalletResponse = core.create_wallet(api_key=api_key)
+    if response.status != status.HTTP_201_CREATED:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
 
 
 @wallets_api.get("/wallets/{address}", status_code=status.HTTP_200_OK)
 def get_wallet(
-    address: str,
-    api_key: str,
-    core: BitcoinWalletCore = Depends(get_core),
-) -> str:
-    return "response"
+        address: str,
+        api_key: str,
+        core: BitcoinWalletCore = Depends(get_core),
+) -> WalletResponse:
+    response: WalletResponse = core.get_wallet(api_key=api_key, address=address)
+    if response.status != status.HTTP_200_OK:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
 
 
-@wallets_api.get("/wallets/{address}/transactions", status_code=status.HTTP_200_OK)
+@wallets_api.get("/wallets/{address}/transactions")
 def get_transactions_for_this_wallet(
-    address: str,
-    api_key: str,
-    core: BitcoinWalletCore = Depends(get_core),
-) -> str:
-    return "response"
+        address: str,
+        api_key: str,
+        core: BitcoinWalletCore = Depends(get_core),
+) -> WalletResponse:
+    response: WalletResponse = core.get_transactions_of_wallet(
+        api_key=api_key, address=address
+    )
+    if response.status != status.HTTP_200_OK:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
+
+
+@wallets_api.post("/wallets/{address}/deposit")
+def deposit(
+        api_key: str,
+        address: str,
+        amount: float,
+        core: BitcoinWalletCore = Depends(get_core),
+) -> WalletResponse:
+    response: WalletResponse = core.deposit(
+        api_key=api_key,
+        address=address,
+        amount_in_usd=amount,
+    )
+    if response.status != status.HTTP_201_CREATED:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
+
+
+@wallets_api.post("/wallets/{address}/withdraw")
+def withdraw(
+        api_key: str,
+        address: str,
+        amount: float,
+        core: BitcoinWalletCore = Depends(get_core),
+) -> WalletResponse:
+    response: WalletResponse = core.withdraw(
+        api_key=api_key,
+        address=address,
+        amount_in_usd=amount,
+    )
+    if response.status != status.HTTP_201_CREATED:
+        raise HTTPException(status_code=response.status, detail=response.msg)
+    return response
